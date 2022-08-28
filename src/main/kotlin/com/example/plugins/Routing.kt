a8be1a46e0b94e3d2489e20e818fb70f08f382fd
Veget-ables <makuhari109@gmail.com>
2022-08-28 22:54:35 +0900

    OrderとそのRoutingを作成

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,9 @@
 package com.example.plugins
 
 import com.example.routes.customerRouting
+import com.example.routes.getOrderRoute
+import com.example.routes.listOrdersRoute
+import com.example.routes.totalizeOrderRoute
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -10,5 +13,8 @@ import io.ktor.server.request.*
 fun Application.configureRouting() {
     routing {
         customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
